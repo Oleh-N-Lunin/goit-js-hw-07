ce8b11b04c814c0709dedb0144490ce9c8086dff
Oleh-N-Lunin <67375261+Oleh-N-Lunin@users.noreply.github.com>
2021-04-09 18:24:16 +0300

    Finished task #4

--- js/task-04.js
+++ js/task-04.js
@@ -7,3 +7,36 @@
 
 */
 
+/* 
+    <div id="counter">
+       <button type="button" data-action="decrement">-1</button>
+       <span id="value">0</span>
+       <button type="button" data-action="increment">+1</button>
+    </div>
+*/
+
+// В спане будем изменять textContent
+
+const refs = {
+    increment: document.querySelector('button[data-action="increment"]'),
+    decrement: document.querySelector('button[data-action="decrement"]'),
+    counterValue: document.querySelector('#value'),
+};
+
+let value = 0;
+
+function render() {
+    refs.counterValue.textContent = value
+};
+
+refs.increment.addEventListener('click', onIncrementClick);
+function onIncrementClick(event) {
+    value += 1;
+    render()
+};
+
+refs.decrement.addEventListener('click', onDecrementClick);
+function onDecrementClick(event) {
+    value -= 1;
+    render()
+};
